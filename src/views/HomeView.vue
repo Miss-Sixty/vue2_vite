b8a7d214942a67a4212f515aafddbb6d0fb35f50
xihe <im0k@outlook.com>
2024-08-21 22:41:09 +0800

    feat: 添加tailwindcss

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,7 @@
-<script setup lang="ts">
-import TheWelcome from '@/components/TheWelcome.vue'
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <main>
-    <TheWelcome />
+    <div class="bg-red-500">aa</div>
   </main>
 </template>

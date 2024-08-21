5eab239fce16a3cafc0afded41297c1bc8638f9e
xihe <im0k@outlook.com>
2024-08-22 00:02:31 +0800

    feat: 更改图片大小

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -11,6 +11,6 @@ const url = ref("狗狗图片");
 <template>
   <main class="flex flex-col items-center pt-10">
     <p class="m-3">{{ url }}：{{ loading ? "加载中..." : "加载完成" }}</p>
-    <img class="size-40" :src="data.message" />
+    <img class="size-64" :src="data.message" />
   </main>
 </template>

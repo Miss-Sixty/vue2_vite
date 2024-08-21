773da35f547395a45015fade46cdc1afbac48bed
xihe <im0k@outlook.com>
2024-08-21 22:07:08 +0800

    feat: 添加sentry

--- src/main.js
+++ src/main.js
@@ -2,8 +2,25 @@ import Vue from 'vue'
 
 import App from './App.vue'
 import router from './router'
-
+import * as Sentry from "@sentry/vue";
 import './assets/main.css'
+
+
+Sentry.init({
+  Vue,
+  dsn: "https://5092eecb0c32e834faf07d1859327e0b@o4507815677984768.ingest.us.sentry.io/4507815680081920",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration(),
+  ],
+  // Tracing
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
 
 new Vue({
   router,
--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,4 @@
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
@@ -6,16 +7,21 @@ import vue2 from '@vitejs/plugin-vue2'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue2(),
-    legacy({
-      targets: ['ie >= 11'],
-      additionalLegacyPolyfills: ['regenerator-runtime/runtime']
-    })
-  ],
+  plugins: [vue2(), legacy({
+    targets: ['ie >= 11'],
+    additionalLegacyPolyfills: ['regenerator-runtime/runtime']
+  }), sentryVitePlugin({
+    org: "wtab",
+    project: "javascript-vue"
+  })],
+
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+
+  build: {
+    sourcemap: true
   }
 })
